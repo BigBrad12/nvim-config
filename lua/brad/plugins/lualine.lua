4569b53ce359b34de2d98c912b39a130f4f686aa
BigBrad12 <bradslater12@gmail.com>
2025-01-30 20:08:25 +0200

    remove plugins not used

--- lua/brad/plugins/lualine.lua
+++ lua/brad/plugins/lualine.lua
@@ -56,12 +56,6 @@ return {
       },
       sections = {
         lualine_x = {
-          {
-            lazy_status.updates,
-            cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
-          },
-          { "encoding" },
           { "fileformat" },
           { "filetype" },
         },
